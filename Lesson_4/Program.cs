6b447ff1154a4c5804b2024ebeb424d5cf259bb3
KU3MI4 <kuzmichov@bk.ru>
2023-06-30 14:50:45 +0300

    add lesson 4

---
+++ Lesson_4/Program.cs
@@ -0,0 +1,45 @@
+﻿// int n = Random.Shared.Next(1000000);
+
+// double s = 0;
+
+// for (int n = 0; n < 15; n++)
+// {
+//     int count = 0;
+//     int i = n;
+//     while (i > 0)
+//     {
+//         count++;
+//         s += i;
+//         i = i / 2;
+//     }
+//     Console.WriteLine($"n:{n}  count:{count}");
+// }
+
+
+// <summary>
+// Сортировка методом выбора
+// </summary>
+// <param name="collection">Исходный массив</param>
+// <returns>Отсортированный массив массив</returns>
+
+int[] SortSelection(int[] collection)
+{
+    int size = collection.Length;
+    for (int i = 0; i < size - 1; i++)
+    {
+        int pos = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (collection[j] < collection[pos]) pos = j;
+        }
+        int temp = collection[i];
+        collection[i] = collection[pos];
+        collection[pos] = temp;
+    }
+    return collection;
+}
+
+var arr = new int[] { 9, 6, 0, 5, 7, 3, 1, 2};
+Console.WriteLine(string.Join(" ", arr));
+SortSelection(arr);
+Console.WriteLine(string.Join(" ", arr));
